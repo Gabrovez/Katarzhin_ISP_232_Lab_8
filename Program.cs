c8627c3ca1a6c2116f2113c0ecf71f85e6d6fc79
Gabrovez <gabrovez321@gmail.com>
2025-10-13 01:41:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Katarzhin_ISP_232_Lab_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
