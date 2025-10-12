860383dd912801909566e5e78259b3cd0fb68fb1
Gabrovez <gabrovez321@gmail.com>
2025-10-13 02:02:05 +0300

    Шаг 1. Свойства 2

--- Person.cs
+++ Person.cs
@@ -8,30 +8,10 @@ namespace Katarzhin_ISP_232_Lab_8
 {
     internal class Person
     {
-        private string name = "Undefined";
-        private int age =  1;
-        public string Name
-        {
-            get
-            {
-                return name;
-            }
-            set
-            {
-                name = value;
-            }
-        }
-        public int Age
-        {
-            set
-            {
-                if (value < 1 || value > 120)
-                    Console.WriteLine("Возраст должен быть в диапазоне от 1 до 120");
-                else
-                    age = value;
-            }
-            get { return age; }
-        }
-
+        string name = "Tom";
+        int age =  1;
+        public int Age { set { age = value; } }
+        public string Name { get { return name; } }
+        public void Print() => Console.WriteLine($"Name: {name} Age: {age}");
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,15 +5,10 @@
         static void Main(string[] args)
         {
             Person person = new Person();
-            person.Name = "Ivan";
-            string personName = person.Name;
-            Console.WriteLine(personName);
-            Person personb = new Person();
-            Console.WriteLine(person.Age);
+            Console.WriteLine(person.Name);
             person.Age = 37;
-            Console.WriteLine(person.Age);
-            person.Age = -37;
-            Console.WriteLine(person.Age);
+            person.Print();
+
 
         }
     }
