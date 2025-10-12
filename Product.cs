4acf4521dde404a26dccba5eda48b0e5c314e4d7
Gabrovez <gabrovez321@gmail.com>
2025-10-13 02:27:26 +0300

    Шаг 2. Мини-проект: Корзина интернет-магазина

---
+++ Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katarzhin_ISP_232_Lab_8
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        private int _quantity;
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (value < 0)
+                    _quantity = 0;
+                else
+                    _quantity = value;
+            }
+        }
+        public decimal TotalPrice
+        {
+            get { return Price * Quantity; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,11 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            Console.WriteLine(person.Name);
-            person.Age = 37;
-            person.Print();
-
+            var apple = new Product { Name = "Яблоко", Price = 10.5m, Quantity = 5 };
+            var banana = new Product { Name = "Банан", Price = 7.2m, Quantity = -2 };
+            Console.WriteLine($"{apple.Name} x {apple.Quantity} = {apple.TotalPrice} Руб.");
+            Console.WriteLine($"{banana.Name} x {banana.Quantity} = {banana.TotalPrice} Руб.");
 
         }
     }
