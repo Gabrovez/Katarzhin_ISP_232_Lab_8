48f1d7fa3c0512a16ee220c3fff3e103e4341fca
Gabrovez <gabrovez321@gmail.com>
2025-10-13 15:27:29 +0300

    Шаг 9. Мини-проект: Система координатных точек

--- Program.cs
+++ Program.cs
@@ -1,22 +1,39 @@
-﻿using System;
-namespace Katarzhin_ISP_232_Lab_8
+﻿namespace Katarzhin_ISP_232_Lab_8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Company microsoft = new("Microsoft");
-            Person tom = new("Tom", microsoft);
-            tom.Print();
+            var p1 = new Point2D(10, 20);
+            var p2 = new Point2D(13, 24);
+            Console.WriteLine($"Расстояние: {p1.Distanceto(p2):F2}");
+        }
+        public struct Point2D
+        {
+            public int X;
+            public int Y;
+            public  Point2D(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+            public double Distanceto(Point2D other)
+            {
+                int dx = X - other.X;
+                int dy = Y - other.Y;
+
+                return Math.Sqrt(dx * dx + dy* dy);
+            }
+            
         }
     }
-}
+ }
 namespace Base
 {
     class Company
     {
         string title;
         public Company(string title) => this.title = title;
-        public void print() => System.Console.WriteLine($"Компания: {title}");
+        public void print() => Console.WriteLine($"Компания: {title}");
     }
 }
