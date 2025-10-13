fbf278d8c86c58c411f68667ac4d75b3673cbef1
Gabrovez <gabrovez321@gmail.com>
2025-10-13 03:12:56 +0300

    Шаг 4. Структуры

--- Program.cs
+++ Program.cs
@@ -4,15 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Hero hero = new Hero("Ясуо", 120);
-            Console.WriteLine($"Имя героя: {hero.Name}\nУровень героя: {hero.Level}\nМана(или заряд пасивки): {hero.Mana}\nСтатус: {hero.Status}");
-            hero.TakeDamage(50);
-            hero.Heal(30);
-            hero.TakeDamage(200);
-            hero.Heal(50);
-            hero.TakeDamage(200);
-
-
+            Persom ahri = new();
+            Persom brand = new("Brand");
+            Persom darius = new("Darius", 25);
+            ahri.Print(); 
+            brand.Print(); 
+            darius.Print(); 
+        }
+        struct Persom
+        {
+            public string name; public int age;
+            public Persom()
+            {
+                name = "Undefined";
+                age = 18;
+            }
+            public Persom(string name = "Tom", int age = 1) 
+            {
+                this.name = name; this.age = age;
+            }
+            public void Print()=> Console.WriteLine($"Имя: {name} Возраст: {age}");
+            
         }
     }
 }
